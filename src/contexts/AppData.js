7d7bd40854708df229fb11bbdf99a8a8ef876fde
GustavoRFS <gustavoribeiro.ribeiro1@gmail.com>
2023-03-17 03:08:45 +0000

    converts class component to function

--- src/contexts/AppData.js
+++ src/contexts/AppData.js
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useMemo, useState } from "react";
 import { Snackbar } from "react-native-paper";
 import api from "../api/api";
 import LoadingModal from "../components/LoadingModal";
@@ -10,78 +10,104 @@ const AppDataContext = createContext({
   bururuTop10: [],
   gururuTop10: [],
   notRated: [],
-  refreshData: undefined,
-  showMessage: undefined,
+  refreshData: () => {},
+  showMessage: () => {},
+  setAllMovies: (newData) => {},
 });
 
 export default AppDataContext;
 
-export class AppDataProvider extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "",
-      snackBarVisibility: false,
-      userData: {},
-      allMovies: [],
-      top10: [],
-      bururuTop10: [],
-      gururuTop10: [],
-      notRated: [],
-      refreshData: undefined,
-      isLoading: true,
-    };
-  }
+export function AppDataProvider(props) {
+  const [isLoading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [snackBarVisibility, setSnackBarVisibility] = useState(false);
+  const [userData, setUserData] = useState({
+    name: "John Doe",
+    profilePic: "https://avatars.githubusercontent.com/u/60088492?v=4",
+  });
+  const [allMovies, setAllMovies] = useState([
+    {
+      title: "shrek",
+      poster_path: "/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
+      release_date: "2001-05-16",
+      average_rating: 9,
+      bururu_rating: 8,
+      gururu_rating: 10,
+      backdrop_path: "/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
+      overview: "shrek",
+    },
+  ]);
+  const top10 = useMemo(
+    () =>
+      allMovies
+        .sort((a, b) => b.average_rating - a.average_rating)
+        .slice(0, 10),
+    [allMovies]
+  );
+  const bururuTop10 = useMemo(
+    () =>
+      allMovies.sort((a, b) => b.bururu_rating - a.bururu_rating).slice(0, 10),
+    [allMovies]
+  );
+  const gururuTop10 = useMemo(
+    () =>
+      allMovies.sort((a, b) => b.gururu_rating - a.gururu_rating).slice(0, 10),
+    [allMovies]
+  );
+  const notRated = useMemo(
+    () => allMovies.filter((movie) => movie.average_rating === 0),
+    [allMovies]
+  );
 
-  refreshData = () => {
+  const refreshData = () => {
     return new Promise(async (resolve) => {
-      this.setState({
-        top10: (await api.get("/movies/average")).data,
-        allMovies: (await api.get("/movies")).data,
-        bururuTop10: (await api.get("/movies/bururu")).data,
-        gururuTop10: (await api.get("/movies/gururu")).data,
-        notRated: (await api.get("/movies/not_rated")).data,
-        userData: (await api.get("/auth/user_data")).data,
-      });
+      // this.setState({
+      //   top10: (await api.get("/movies/average")).data,
+      //   allMovies: (await api.get("/movies")).data,
+      //   bururuTop10: (await api.get("/movies/bururu")).data,
+      //   gururuTop10: (await api.get("/movies/gururu")).data,
+      //   notRated: (await api.get("/movies/not_rated")).data,
+      //   userData: (await api.get("/auth/user_data")).data,
+      // });
       resolve();
     });
   };
 
-  componentDidMount() {
-    this.refreshData().then(() => {
-      this.setState({ isLoading: false });
-    });
-  }
-
-  showMessage = (text) => {
-    this.setState({ message: text, snackBarVisibility: true });
+  const showMessage = (text) => {
+    setMessage(text);
+    setSnackBarVisibility(true);
   };
 
-  render(props) {
-    return (
-      <AppDataContext.Provider
-        value={{
-          userData: this.state.userData,
-          allMovies: this.state.allMovies,
-          top10: this.state.top10,
-          bururuTop10: this.state.bururuTop10,
-          gururuTop10: this.state.gururuTop10,
-          notRated: this.state.notRated,
-          refreshData: this.refreshData,
-          showMessage: this.showMessage,
-        }}
+  useEffect(() => {
+    refreshData().then(() => {
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <AppDataContext.Provider
+      value={{
+        userData,
+        allMovies,
+        top10,
+        bururuTop10,
+        gururuTop10,
+        notRated,
+        refreshData,
+        showMessage,
+        setAllMovies,
+      }}
+    >
+      <Snackbar
+        visible={snackBarVisibility}
+        onDismiss={() => setSnackBarVisibility(false)}
+        duration={2000}
+        style={{ backgroundColor: "#3f3f3f" }}
       >
-        <Snackbar
-          visible={this.state.snackBarVisibility}
-          onDismiss={() => this.setState({ snackBarVisibility: false })}
-          duration={2000}
-          style={{ backgroundColor: "#3f3f3f" }}
-        >
-          {this.state.message}
-        </Snackbar>
-        <LoadingModal visible={this.state.isLoading} />
-        {this.props.children}
-      </AppDataContext.Provider>
-    );
-  }
+        {message}
+      </Snackbar>
+      <LoadingModal visible={isLoading} />
+      {props.children}
+    </AppDataContext.Provider>
+  );
 }
